260f5469e159ac4065c7ebbf3bacb49f8bc2a212
AlvaroG001 <alvarogarciaortiz7@gmail.com>
2024-04-26 00:53:00 +0200

    Arreglo Elections

--- src/components/Elections/Elections.js
+++ src/components/Elections/Elections.js
@@ -23,9 +23,9 @@ function Elections({ logout }) {
         const userDataString = localStorage.getItem('userData');
         const userData = JSON.parse(userDataString);
         const communityId = userData.comunidad.id; // Asegúrate de que el objeto y la propiedad sean correctos
-        
+
         setPresident(userData?.president);
-    
+
         try {
             const response = await fetch(`http://localhost:9000/api/votaciones?communityId=${communityId}`);
             if (response.ok) {
@@ -61,7 +61,7 @@ function Elections({ logout }) {
         const userDataString = localStorage.getItem('userData');
         const userData = JSON.parse(userDataString);
         const userId = userData.id;
-    
+
         const voteOptions = {
             'agree': 1,
             'disagree': 2,
@@ -72,16 +72,16 @@ function Elections({ logout }) {
         let updatedElection = { ...selectedElection };
         updatedElection.votantes = [...updatedElection.votantes, userId];  // Añadir el ID del usuario al array de votantes
         updatedElection.respuestas = [...updatedElection.respuestas, voteOption];  // Añadir la opción de voto al array de respuestas
-    
+
         // Actualizar los contadores localmente antes de enviar la solicitud
         const updatedAgreeCount = agreeCount + (voteType === 'agree' ? 1 : 0);
         const updatedDisagreeCount = disagreeCount + (voteType === 'disagree' ? 1 : 0);
         const updatedAbstainCount = abstainCount + (voteType === 'abstain' ? 1 : 0);
-    
+
         setAgreeCount(updatedAgreeCount);
         setDisagreeCount(updatedDisagreeCount);
         setAbstainCount(updatedAbstainCount);
-    
+
         try {
             const response = await fetch(`http://localhost:9000/api/votaciones/${selectedElection.id}/vote`, {
                 method: 'POST',
@@ -89,33 +89,33 @@ function Elections({ logout }) {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(updatedElection)  // Enviar la elección actualizada
-            }); 
+            });
             if (response.ok) {
                 setHasVoted(true);
                 alert('Your vote has been recorded.');
                 setSelectedElection(updatedElection);  // Actualizar la elección en el estado
-                
+
             } else {
                 throw new Error('Failed to record the vote');
             }
         } catch (error) {
             console.error('Error submitting vote:', error);
         }
-        
+
         fetchElections();
     };
-    
-    
+
+
 
     const selectElection = (election) => {
         setSelectedElection(election);
-        checkIfUserHasVoted(election.votantes);            
+        checkIfUserHasVoted(election.votantes);
         setAgreeCount(election.respuestas.filter(respuesta => respuesta === '1').length);
         setDisagreeCount(election.respuestas.filter(respuesta => respuesta === '2').length);
-        setAbstainCount(election.respuestas.filter(respuesta => respuesta === '3').length);  
+        setAbstainCount(election.respuestas.filter(respuesta => respuesta === '3').length);
     };
 
-    
+
 
     const deleteElection = async (electionId) => {
         try {
@@ -140,93 +140,101 @@ function Elections({ logout }) {
         }
     };
 
- return (
-    <div className="home-container">
-        <aside className="sidebar">
-            <Link to="/home">
-                <img src={homeButtonImg} alt="Home" className="home-button" />
-            </Link>
-            <span className="sidebar-label">Inicio</span>
-            <Link to="/reservations">
-                <img src={calendarButtonImg} alt="Calendar" className="calendar-button" />
-            </Link>
-            <span className="sidebar-label">Reservas</span>
-            <Link to="/meetings">
-                <img src={meetingButtonImg} alt="Meeting" className="meeting-button" />
-            </Link>
-            <span className="sidebar-label">Reuniones</span>
-            <Link to="/votes">
-                <img src={voteButtonImg} alt="Vote" className="vote-button" />
-            </Link>
-            <span className="sidebar-label">Votaciones</span>
-            <Link to="/chats">
-                <img src={chatButtonImg} alt="Chat" className="chat-button" />
-            </Link>
-            <span className="sidebar-label">Chats</span>
+    const formatDate = (dateString) => {
+        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+        return new Date(dateString).toLocaleDateString("es-ES", options);
+    };
 
-            {president && (
-                <>
-                    <Link to="/settings">
-                        <img src={settingsButtonImg} alt="Settings" className="settings-button" />
-                    </Link>
-                    <span className="sidebar-label">Ajustes</span>
-                </>
-            )}
-        </aside>
 
-        <main className="main-content">
-            <header className="main-header">
-                <h1>Votaciones de la Comunidad</h1>
-                <div className="header-buttons"> 
-                <Link to="/create-elections" className="create-elections-button">
-                    Crear Votación
+    return (
+        <div className="home-container">
+            <aside className="sidebar">
+                <Link to="/home">
+                    <img src={homeButtonImg} alt="Home" className="home-button" />
                 </Link>
-                
-                   <button onClick={logout} className="logout-button">Cerrar sesión</button>
-                </div>
-            </header>
-            <div className="election-details">
-                {selectedElection && (
-                    <>
-                        <h2>{selectedElection.titulo}</h2>
-                        <p>{selectedElection.informacion}</p>
-                        <p>{selectedElection.fecha}</p>
-                        {!hasVoted ? (
-                            <div>
-                                <button onClick={() => handleVote('agree')}>Estoy de acuerdo</button>
-                                <button onClick={() => handleVote('disagree')}>No estoy de acuerdo</button>
-                                <button onClick={() => handleVote('abstain')}>Me abstengo</button>
-                            </div>
-                        ) : (
-                            <p>Thanks for voting!</p>
-                        )}
-                        {president && (
-                            <button onClick={() => deleteElection(selectedElection.id)}>Eliminar Votación</button>
-                        )}
+                <span className="sidebar-label">Inicio</span>
+                <Link to="/reservations">
+                    <img src={calendarButtonImg} alt="Calendar" className="calendar-button" />
+                </Link>
+                <span className="sidebar-label">Reservas</span>
+                <Link to="/meetings">
+                    <img src={meetingButtonImg} alt="Meeting" className="meeting-button" />
+                </Link>
+                <span className="sidebar-label">Reuniones</span>
+                <Link to="/votes">
+                    <img src={voteButtonImg} alt="Vote" className="vote-button" />
+                </Link>
+                <span className="sidebar-label">Votaciones</span>
+                <Link to="/chats">
+                    <img src={chatButtonImg} alt="Chat" className="chat-button" />
+                </Link>
+                <span className="sidebar-label">Chats</span>
 
-                        {president && (
-                            <div>
-                                <h2>Resultados de la Votación</h2>
-                                <p>Estoy de acuerdo: {agreeCount}</p>
-                                <p>No estoy de acuerdo: {disagreeCount}</p>
-                                <p>Me abstengo: {abstainCount}</p>
-                            </div>
-                        )} 
+                {president && (
+                    <>
+                        <Link to="/settings">
+                            <img src={settingsButtonImg} alt="Settings" className="settings-button" />
+                        </Link>
+                        <span className="sidebar-label">Ajustes</span>
                     </>
                 )}
-            </div>
+            </aside>
 
-            <div className="elections-list">
-                <h2>Previous Elections</h2>
-                {elections.map(election => (
-                    <button key={election.id} onClick={() => selectElection(election)}>
-                        {election.titulo} - {election.fecha}
-                    </button>
-                ))}
-            </div>
-        </main>
-    </div>
-);
+            <main className="main-content">
+                <header className="main-header">
+                    <h1>Votaciones de la Comunidad</h1>
+                    <div className="header-buttons">
+                        <Link to="/create-elections" className="create-meeting-button">
+                            Crear Votación
+                        </Link>
+
+                        <button onClick={logout} className="logout-button">Cerrar sesión</button>
+                    </div>
+                </header>
+                <div className="elections-container">
+                    {selectedElection && (
+                        <div className="elections-details">
+                            <h2 className="vote-title" >{selectedElection.titulo}</h2>
+                            <p className="election-info">{selectedElection.informacion}</p>
+                            <h4 className="election-info">Cierre de la votación: </h4>
+                            <p className="election-info">{formatDate(selectedElection.fecha)}</p>
+                            {!hasVoted ? (
+                                <div className="vote-container">
+                                    <button className="vote-button-election" onClick={() => handleVote('agree')}>Estoy de acuerdo</button>
+                                    <button className="vote-button-election" onClick={() => handleVote('disagree')}>No estoy de acuerdo</button>
+                                    <button className="vote-button-election" onClick={() => handleVote('abstain')}>Me abstengo</button>
+                                </div>
+                            ) : (
+                                <h3 className='thanks'>Thanks for voting!</h3>
+                            )}
+                            {president && (
+                                <button className="delete-election-button" onClick={() => deleteElection(selectedElection.id)}>Eliminar Votación</button>
+                            )}
+
+                            {president && (
+                                <div className="voting-results">
+                                    <h3>Resultados de la Votación</h3>
+                                    <p>Estoy de acuerdo  =  {agreeCount}</p>
+                                    <p>No estoy de acuerdo  =  {disagreeCount}</p>
+                                    <p>Me abstengo  =  {abstainCount}</p>
+                                </div>
+                            )}
+                        </div>
+                    )}
+
+                    <div className="elections-list">
+                        <h2>Votaciones pedientes</h2>
+                        {elections.map(election => (
+                            <button className="ListMeeting-button" key={election.id} onClick={() => selectElection(election)}>
+                                {election.titulo}
+                            </button>
+                        ))}
+                    </div>
+                </div>
+
+            </main>
+        </div>
+    );
 }
 
 export default Elections;
